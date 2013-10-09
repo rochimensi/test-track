b0d69a04d47e7c1bcdec48578ec20b89aa98e467
Mariano Ravinale <mravinale@gmail.com>
2013-10-09 17:07:13 -0300

    added TestTrackContextCustomInitializer for db creation code first

--- TestTrack/TestTrack/Global.asax.cs
+++ TestTrack/TestTrack/Global.asax.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TestTrack.Models;
 
 namespace TestTrack
 {
@@ -16,6 +18,8 @@ namespace TestTrack
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new TestTrackContextCustomInitializer());
+
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
