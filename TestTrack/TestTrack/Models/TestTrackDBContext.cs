fbc6a137d5a882f19ec8cc188d9deb8a061537a1
matiasbeckerle <matiasbeckerle@gmail.com>
2013-10-10 09:44:40 -0300

    Added TestTrackContextCustomInitializer
    Added TestTrackContextCustomInitializer for db creation code first.
    
    This is a merge of pull request #1 by @mravinale.
    Commit b0d69a04d47e7c1bcdec48578ec20b89aa98e467.

--- TestTrack/TestTrack/Models/TestTrackDBContext.cs
+++ TestTrack/TestTrack/Models/TestTrackDBContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 
@@ -20,6 +21,9 @@ namespace TestTrack.Models
         public DbSet<TestRun> TestRuns { get; set; }
         public DbSet<Result> Results { get; set; }
         public DbSet<Defect> Defects { get; set; }
+
+        public TestTrackDBContext(){ }
+        public TestTrackDBContext(string connString) : base(connString) { }
 
         public override int SaveChanges()
         {
@@ -44,6 +48,35 @@ namespace TestTrack.Models
             }
 
             return base.SaveChanges();
+        }
+    }
+
+    public class TestTrackContextCustomInitializer : IDatabaseInitializer<TestTrackDBContext>
+    {
+        public void InitializeDatabase(TestTrackDBContext context)
+        {
+            if (context.Database.Exists() && context.Database.CompatibleWithModel(true)) return;
+
+            context.Database.Delete();
+            context.Database.Create();
+
+            context.Projects.AddOrUpdate(p => p.Title,
+                new Project
+                {
+                    ProjectID = 1,
+                    Title = "Doppler",
+                    Description = "Create, send, analyze & optimize your Email Marketing campaigns in a effective way. Find out more about the easiest Email Marketing app ever!",
+                    CreatedOn = DateTime.Now
+                },
+                new Project
+                {
+                    ProjectID = 2,
+                    Title = "Lander",
+                    Description = "Lander lets you create beautiful landing pages for your social media, email and online marketing campaigns using an easy step-by-step process.",
+                    CreatedOn = DateTime.Now
+                }
+            );
+            context.SaveChanges();
         }
     }
 }
