da043f6e8d25a7469affe777f7f5dd8e7a1040de
matiasbeckerle <matiasbeckerle@gmail.com>
2013-10-10 15:19:15 -0300

    Removed Account persistance functionality
    Also changed the LogOff action to use FormsAuthentication sign out.

--- TestTrack/TestTrack/Global.asax.cs
+++ TestTrack/TestTrack/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -27,6 +28,8 @@ namespace TestTrack
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+
+            //WebSecurity.
         }
     }
 }
