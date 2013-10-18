4c7c78d3afee4d00b88a1bc3e6ac421973d59f83
rochimensi <rochibass@gmail.com>
2013-10-18 10:08:56 -0300

    Sequence contains no elements fixed again
    As the Post for ProjectDropdown redirects to the current URL, it might
    be possible that the user is at /TestPlanPerIteration/Index/3 and
    changes the project, but for the new project there is no IterationID 3.
    So the url is set to redirect to /TestPlanPerIteration/Index.

--- TestTrack/TestTrack/Controllers/TestPlanPerIterationController.cs
+++ TestTrack/TestTrack/Controllers/TestPlanPerIterationController.cs
@@ -37,10 +37,22 @@ namespace TestTrack.Controllers
             // An iteration was selected by the user
             if (id > 0)
             {
-                iteration = (from i in db.Iterations
-                             where i.IterationID == id && i.ProjectID == userSettings.workingProject
-                             orderby i.DueDate descending 
-                             select i).ToList().First();
+                var itForProject = from i in db.Iterations
+                                   where i.IterationID == id && i.ProjectID == userSettings.workingProject
+                                   orderby i.DueDate descending
+                                   select i;
+
+                if (itForProject.Count() > 0)
+                {
+                    iteration = itForProject.ToList().First();
+                }
+                else
+                {
+                    string url = Request.Url.AbsoluteUri;
+                    int lastPath = url.LastIndexOf('/');
+                    url = url.Substring(0, lastPath);
+                    return Redirect(url);
+                }
             }
             return View(iteration);
         }
